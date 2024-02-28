f8e6c2ecdfca5f87e0b582cc113c1ec14f4c2f58
Fernando Isidro Luna <luffynando@gmail.com>
2024-02-27 21:33:55 -0600

    build: :construction: prepare for usage build with vite in lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: 'src/index.ts',
+      name: 'baseConverter',
+      fileName: 'base_converter',
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
   test: {
     globals: true,
     coverage: {
