1c0b920837b7a947507fff1c9e8b594f95784934
Fernando Isidro Luna <luffynando@gmail.com>
2024-07-12 23:16:28 -0600

    chore: :hammer: update development script to publish and build manually on local not in ci

--- index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BaseConverter } from './src/base_converter.js';
-export { default as BaseConverterSequence } from './src/base_converter_sequence.js';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as BaseConverter } from './base_converter.js';
+export { default as BaseConverterSequence } from './base_converter_sequence.js';
